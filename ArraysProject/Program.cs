f9a4d4709b68cbb2f25b7929ab0853ea91f9b213
yunioseijo <yunioseijo@gmail.com>
2023-12-13 01:01:41 +0100

    Inicializacion de un array

--- ArraysProject/Program.cs
+++ ArraysProject/Program.cs
@@ -1,9 +1,13 @@
-﻿string[] fraudulentOrderIDs = new string[3];
+﻿/*
+string[] fraudulentOrderIDs = new string[3];
 
 fraudulentOrderIDs[0] = "A123";
 fraudulentOrderIDs[1] = "B456";
 fraudulentOrderIDs[2] = "C789";
 // fraudulentOrderIDs[3] = "D000";
+*/
+
+string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
 
 Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
 Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
