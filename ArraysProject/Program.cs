88a1dc4bc54620c586db4c3b36c26e6a97570daa
yunioseijo <yunioseijo@gmail.com>
2023-12-13 01:06:52 +0100

    Using length

--- ArraysProject/Program.cs
+++ ArraysProject/Program.cs
@@ -16,3 +16,5 @@ Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
 fraudulentOrderIDs[0] = "F000";
 
 Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
