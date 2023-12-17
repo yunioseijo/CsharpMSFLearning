f7b09562a809d3a1ae7d63d40f5e9bfaca51fa47
yunioseijo <yunioseijo@gmail.com>
2023-12-18 00:50:00 +0100

    Iterar a traves de los nombres y sumar las notas de todos los estudiantes asignando letras segun puntuación

--- Foreach-array-if-array/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Foreach-array-if-array/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4190d8b8cdc8c3209401d13dfcfcb486640768ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1929c2a636b06d6245644a3a9ae529f5295fefc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
